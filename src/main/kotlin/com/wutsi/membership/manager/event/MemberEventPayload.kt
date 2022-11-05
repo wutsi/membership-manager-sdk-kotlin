c1773ed18b58fbf0e50e99c572bf1cb5042e3917
wutsi <herve.tchepannou@wutsi.com>
2022-11-05 18:41:00 -0400

    make properties public

--- src/main/kotlin/com/wutsi/membership/manager/event/MemberEventPayload.kt
+++ src/main/kotlin/com/wutsi/membership/manager/event/MemberEventPayload.kt
@@ -1,7 +1,7 @@
 package com.wutsi.membership.manager.event
 
 data class MemberEventPayload(
-    private val accountId: Long = -1,
-    private val phoneNumber: String = "",
-    private val pin: String? = null
+    val accountId: Long = -1,
+    val phoneNumber: String = "",
+    val pin: String? = null
 )
