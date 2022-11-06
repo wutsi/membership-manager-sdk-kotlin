f1c879af6650fcb39b3417ca9da502ab4128533b
wutsi <herve.tchepannou@wutsi.com>
2022-11-06 08:09:42 -0500

    add errors

--- src/main/kotlin/com/wutsi/membership/manager/error/ErrorURN.kt
+++ src/main/kotlin/com/wutsi/membership/manager/error/ErrorURN.kt
@@ -1,5 +1,8 @@
 package com.wutsi.membership.manager.error
 
 enum class ErrorURN(val urn: String) {
-    MEMBER_ALREADY_REGISTERED("urn:wutsi:error:membership-manager:member-already-registered")
+    MEMBER_ALREADY_REGISTERED("urn:wutsi:error:membership-manager:member-already-registered"),
+    MEMBER_ALREADY_SUSPENDED("urn:wutsi:error:membership-manager:member-already-suspended"),
+    MEMBER_NOT_ACTIVE("urn:wutsi:error:membership-manager:member-not-active"),
+    MEMBER_NOT_FOUND("urn:wutsi:error:membership-manager:member-not-found")
 }
